715793369a7b1755a19be66ce9abdc74eb148181
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-07-15 19:43:39 -0300

    j

--- portal/src/app/app.routes.ts
+++ portal/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Local } from './local/local';
 import { Regional } from './regional/regional';
 import { Internacional } from './internacional/internacional';
-
+import { Somos } from './somos/somos';
 
 export const routes: Routes = [
-    {path:'local',component:Local},
-    {path:'regional',component:Regional},
-    {path:'internacional',component:Internacional}
+    { path: 'local', component: Local },
+    { path: 'regional', component: Regional },
+    { path: 'internacional', component: Internacional },
+    { path: 'quem-somos', component: Somos }
 ];
--- portal/src/app/app.ts
+++ portal/src/app/app.ts
@@ -2,12 +2,10 @@ import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
-import { Home } from './home/home';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer,Home],
+  imports: [RouterOutlet,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
