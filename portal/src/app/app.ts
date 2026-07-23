7f3a10848d615930c81d83623057522ef6bbe7b3
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-07-22 21:14:46 -0300

    j

--- portal/src/app/app.ts
+++ portal/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer],
+  imports: [RouterOutlet,Header,Footer,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
