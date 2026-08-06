feab3b3e1e237b8539700c555cf8b57c905a61d2
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-08-05 21:01:45 -0300

    j

--- portal/src/app/app.routes.ts
+++ portal/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { Local } from './local/local';
 import { Regional } from './regional/regional';
 import { Internacional } from './internacional/internacional';
 import { Somos } from './somos/somos';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    { path: 'home', component: Home },
     { path: 'local', component: Local },
     { path: 'regional', component: Regional },
     { path: 'internacional', component: Internacional },
