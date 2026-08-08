c843299b439b064506a862a4f88c1d6ec3ac1a40
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-08-07 21:24:44 -0300

    j

--- portal/src/app/app.routes.ts
+++ portal/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { Visita } from './visita/visita';
 import { Acontecendo } from './acontecendo/acontecendo';
 import { Sobre } from './sobre/sobre';
+import { Ingressos } from './ingressos/ingressos';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: 'visita', component: Visita },
     { path: 'acontecendo', component: Acontecendo },
-    { path: 'sobre', component: Sobre }
+    { path: 'sobre', component: Sobre },
+    { path: 'ingressos', component: Ingressos }
 ];
